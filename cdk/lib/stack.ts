fc1a3cda334544f8a2cd426ffacd388f6a2817bd
Takehiro Hagiwara <hagiwara.takehiro@classmethod.jp>
2022-07-25 17:18:20 +0900

    feat(lambda): add x-ray trace to dynamodb

--- cdk/lib/stack.ts
+++ cdk/lib/stack.ts
@@ -1,4 +1,4 @@
-import { aws_apigateway as apigw, aws_dynamodb as ddb, aws_lambda as lambda, aws_iam as iam, Stack, StackProps } from 'aws-cdk-lib'
+import { aws_apigateway as apigw, aws_dynamodb as ddb, aws_lambda as lambda, aws_iam as iam, Stack, StackProps, Duration } from 'aws-cdk-lib'
 import { Construct } from 'constructs'
 import { Tracing } from 'aws-cdk-lib/aws-lambda'
 
@@ -24,24 +24,42 @@ export async function buildStack(
     },
   })
 
-  // lambda
-  const func = new lambda.Function(stack, 'RustLambdaSampleFunction', {
+  // rust lambda
+  const rustFunc = new lambda.Function(stack, 'RustLambdaSampleFunction', {
     functionName: 'RustLambdaSampleFunction',
     runtime: lambda.Runtime.PROVIDED_AL2,
     handler: 'RustLambdaSampleFunction',
-    code: lambda.Code.fromAsset(`${__dirname}/../../lambda/hello/target/cdk/release`),
+    code: lambda.Code.fromAsset(`${__dirname}/../../lambda/rust-sample/target/cdk/release`),
     tracing: Tracing.ACTIVE,
     insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_135_0
   })
 
-  func.addToRolePolicy(new iam.PolicyStatement({
+  rustFunc.addToRolePolicy(new iam.PolicyStatement({
     effect: iam.Effect.ALLOW,
     resources: [table.tableArn],
     actions: ['dynamodb:Query', 'dynamodb:GetItem', 'dynamodb:UpdateItem']
   }))
 
-  // rest method
-  restApi.root.addResource('hello').addMethod('GET', new apigw.LambdaIntegration(func))
+  restApi.root.addResource('rust').addMethod('GET', new apigw.LambdaIntegration(rustFunc))
+
+  // node lambda
+  const nodeFunc = new lambda.Function(stack, 'NodeLambdaSampleFunction', {
+    functionName: 'NodeLambdaSampleFunction',
+    runtime: lambda.Runtime.NODEJS_16_X,
+    handler: 'index.handler',
+    timeout: Duration.seconds(15),
+    code: lambda.Code.fromAsset(`${__dirname}/../../lambda/node-sample`),
+    tracing: Tracing.ACTIVE,
+    insightsVersion: lambda.LambdaInsightsVersion.VERSION_1_0_135_0
+  })
+
+  nodeFunc.addToRolePolicy(new iam.PolicyStatement({
+    effect: iam.Effect.ALLOW,
+    resources: [table.tableArn],
+    actions: ['dynamodb:Query', 'dynamodb:GetItem', 'dynamodb:UpdateItem']
+  }))
+
+  restApi.root.addResource('node').addMethod('GET', new apigw.LambdaIntegration(nodeFunc))
 
   return stack
 }
--- lambda/node-sample/index.mjs
+++ lambda/node-sample/index.mjs
@@ -1,35 +1,27 @@
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
-import { DynamoDBDocumentClient, GetCommand } from '@aws-sdk/lib-dynamodb'
-// import { captureAWSv3Client } from 'aws-xray-sdk'
-//
-// const patchedDynamoDBClient = captureAWSv3Client(
-//   new DynamoDBClient({
-//     apiVersion: '2012-08-10',
-//     region: 'ap-northeast-1',
-//   })
-// )
-//
-// const client = DynamoDBDocumentClient.from(patchedDynamoDBClient)
+import { DynamoDBClient, GetItemCommand } from '@aws-sdk/client-dynamodb'
+import pkg from 'aws-xray-sdk'
 
-const client = DynamoDBDocumentClient.from(
-  new DynamoDBClient({
-    apiVersion: '2012-08-10',
-    region: 'ap-northeast-1',
-  })
-)
+const { captureAWSv3Client } = pkg;
+
+console.log(`func<${captureAWSv3Client}>`)
+
+const client = captureAWSv3Client(new DynamoDBClient({
+  region: 'ap-northeast-1'
+}))
 
 export async function handler(event) {
+  const params = JSON.stringify(event.multiValueQueryStringParameters)
   const msg = await getItem()
   return {
     statusCode: 200,
-    body: `node-sample ${event.name || 'stranger'}, ${msg}`
+    body: `node-sample ${params}, ${msg}`
   }
 }
 
 async function getItem() {
-  const res = await client.send(new GetCommand({
+  const res = await client.send(new GetItemCommand({
     TableName: 'RustLambdaSampleTable',
-    Key: { Id: { S: 'rust-sample' } }
+    Key: { Id: { S: 'node-sample' } }
   }))
   return JSON.stringify(res.Item)
 }
